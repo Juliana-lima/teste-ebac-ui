2db3d1cfb6ed37374f0d2bed5f6ad23494262e3f
julia <julianne.1407@gmail.com>
2022-02-14 23:18:12 -0300

    Utilizando o Faker com Cypress

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,8 +2,15 @@
 
 context('Funcionalidade Login', () =>{
 
-    it('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () =>{
         cy.get('#username').type('mariateste1@teste.com.br')
         cy.get('#password').type('422070g#hLX')
         cy.get('.woocommerce-form > .button').click()
@@ -12,7 +19,6 @@ context('Funcionalidade Login', () =>{
     })  
     
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('mariste2@teste.com.br')
         cy.get('#password').type('422070g#hLX')
         cy.get('.woocommerce-form > .button').click()
@@ -22,7 +28,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('400000LX')
         cy.get('.woocommerce-form > .button').click()
