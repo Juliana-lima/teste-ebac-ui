808b16828feefea7f55ebd2e272d43bc22d6e0ec
julia <julianne.1407@gmail.com>
2022-02-15 00:13:31 -0300

    Trabalhando com listas e variáveis

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -8,14 +8,20 @@ describe('Funcionalidade Pré-Cadastro', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
 
-    it('Deve completar o pré-cadastro com sucesso', () => {      
-        cy.get('#reg_email').type(faker.internet.email())
+    it('Deve completar o pré-cadastro com sucesso', () => {   
+        
+        let firstNameFaker = faker.name.firstName()
+        let latNameFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(firstNameFaker)
+    
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('teste#teste')
         cy.get(':nth-child(4) > .button').click()
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
         cy.get('.page-title').should('contain', 'Detalhes da conta')
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(firstNameFaker)
+        cy.get('#account_last_name').type(latNameFaker)
         cy.get('.woocommerce-Button').click()
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
 
